c382340ff1ca6f604c50aae3e6c0cbf30f7a9007
Ian Haranaka <ianhrnk@gmail.com>
2020-11-17 01:02:31 -0400

    finalizado!

--- src/solucao_inicial.cu
+++ src/solucao_inicial.cu
@@ -1,24 +1,31 @@
-/********************************************************************
+/********************************************************************************
+ * Trabalho 2: Programação Paralela para Processador Many-core (GPU) Usando CUDA
  * Aluno: Ian Haranaka | RGA: 2018.1904.009-7
  * Comando de compilação: nvcc solucao_inicial.cu -o solucao_inicial
- ********************************************************************/
+ ********************************************************************************/
 
 #include <iostream>
 #include <fstream>
 #include <string>
 
+char *AlocaSequencia(int n);
+__global__ void DistanciaEdicao(int n, int m, char *S, char *R, int *D);
+
 int main(int argc, char *argv[])
 {
   std::ifstream entrada (argv[1]);
-  std::string S, R;
   int n, m, resultado;
+  char *h_S, *h_R;
 
   // Leitura do arquivo de entrada
   if (entrada.is_open())
   {
-    entrada >> n >> m >> S >> R;
-    S.insert(S.begin(), ' ');
-    R.insert(R.begin(), ' ');
+    entrada >> n >> m;
+
+    h_S = AlocaSequencia(n);
+    h_R = AlocaSequencia(m);
+    entrada >> &(h_S[1]) >> &(h_R[1]);
+
     entrada.close();
   }
   else
@@ -27,34 +34,85 @@ int main(int argc, char *argv[])
     return -1;
   }
 
-  int nElementos = (n+1) * (m+1);
-  int nBytes = nElementos * sizeof(int);
+  int tamanho_grid = (n+1) * (m+1);
 
-  // Aloca matriz no host
-  int *h_D = (int *) malloc(nBytes);
-
-  // Inicializa matriz
-  for (int i = 0, j = 0; i <= nElementos; i += m+1, ++j)
-    h_D[i] = j;
-  for (int j = 0; j <= m; ++j)
-    h_D[j] = j;
-
-  // Aloca matriz na memória global do kernel
+  // Aloca matriz e strings na memória global do kernel
   int *d_D;
-  cudaMalloc(&d_D, nBytes);
+  char *d_S, *d_R;
+  cudaMalloc(&d_D, tamanho_grid * sizeof(int));
+  cudaMalloc(&d_S, (n+1) * sizeof(char));
+  cudaMalloc(&d_R, (m+1) * sizeof(char));
 
-  // Copia a matriz do host para a GPU
-  cudaMemcpy(d_D, h_D, nBytes, cudaMemcpyHostToDevice);
+  // Copia as strings para a memória global do kernel
+  cudaMemcpy(d_S, h_S, (n+1) * sizeof(char), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_R, h_R, (m+1) * sizeof(char), cudaMemcpyHostToDevice);
 
   // Invoca o kernel
   int threadsPerBlock = n; // No máximo 1024
-  //NomeDaFuncao<<<1, threadsPerBlock>>>(<params>);
+  DistanciaEdicao<<<1, threadsPerBlock>>>(n, m, d_S, d_R, d_D);
 
-  //cudaMemcpy(resultado, d_D, sizeof(int), cudaMemcpyDeviceToHost);
+  cudaDeviceSynchronize();
 
-  // Libera matrizes
-  free(h_D);
+  cudaMemcpy(&resultado, &d_D[tamanho_grid - 1], sizeof(int), cudaMemcpyDeviceToHost);
+  std::cout << resultado << std::endl;
+
+  // Libera matriz e strings
+  free(h_S);
+  free(h_R);
   cudaFree(d_D);
+  cudaFree(d_S);
+  cudaFree(d_R);
 
   return 0;
+}
+
+char *AlocaSequencia(int n)
+{
+	char *seq;
+
+	seq = (char *) malloc((n+1) * sizeof(char));
+	if (seq == NULL)
+	{
+    std::cout << "Erro na alocação de estruturas!\n";
+		std::exit(1);
+	}
+  seq[0] = ' ';
+	return seq;
+}
+
+__global__ void DistanciaEdicao(int n, int m, char *S, char *R, int *D)
+{
+  int a, b, c, t, min;
+  int nADiag = n + m - 1;
+  int i = threadIdx.x + 1, j;
+
+  // Inicialização da matriz
+  if (threadIdx.x == 0)
+  {
+    for (int i = 0, j = 0; i < (n+1) * (m+1); i += m+1, ++j)
+      D[i] = j;
+    for (int j = 0; j <= m; ++j)
+      D[j] = j;
+  }
+
+  __syncthreads();
+
+  // Para cada anti-diagonal
+	for (int aD = 2; aD <= nADiag + 1; aD++)
+	{
+    j = aD - i;
+    // Se é uma célula válida
+    if (j > 0 && j <= m)
+    {
+      t = (S[i] != R[j] ? 1 : 0);
+      a = D[i*(m+1)+j-1] + 1;
+      b = D[(i-1)*(m+1)+j] + 1;
+      c = D[(i-1)*(m+1)+j-1] + t;
+      // Calcula d[i][j] = min(a, b, c)
+      min = (a < b ? a : b);
+      min = (c < min ? c : min);
+      D[i*(m+1)+j] = min;
+    }
+    __syncthreads();
+  }
 }
