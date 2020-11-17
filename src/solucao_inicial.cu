8539cabbd88b292deecae87d81ccac720cafedbe
Ian Haranaka <ianhrnk@gmail.com>
2020-11-17 15:31:59 -0400

    solucao inicial finalizada

--- src/solucao_inicial.cu
+++ src/solucao_inicial.cu
@@ -8,9 +8,11 @@
 #include <iostream>
 #include <fstream>
 
+const int MAX_THREADS = 1024;
+
 char *AlocaSequencia(int n);
 __global__ void InicializaVetor(int n, int m, int *D);
-__global__ void DistanciaEdicao(int n, int m, char *S, char *R, int *D);
+__global__ void CalculaDistanciaEdicao(int n, int m, char *S, char *R, int *D);
 
 int main(int argc, char *argv[])
 {
@@ -31,46 +33,44 @@ int main(int argc, char *argv[])
   }
   else
   {
-    std::cout << "Arquivo não encontrado!\n";
+    std::cout << "Arquivo não encontrado!" << std::endl;
     return -1;
   }
 
-  int tamanho_grid = (n+1) * (m+1);
+  int tam_vetor = (n+1) * (m+1);
 
-  // Aloca matriz e strings na memória global do kernel
+  // Aloca o vetor e as strings na memória global da GPU
   int *d_D;
   char *d_S, *d_R;
-  cudaMalloc(&d_D, tamanho_grid * sizeof(int));
+  cudaMalloc(&d_D, tam_vetor * sizeof(int));
   cudaMalloc(&d_S, (n+2) * sizeof(char));
   cudaMalloc(&d_R, (m+2) * sizeof(char));
 
-  // Copia as strings para a memória global do kernel
+  // Copia as strings para a memória global da GPU
   cudaMemcpy(d_S, h_S, (n+2) * sizeof(char), cudaMemcpyHostToDevice);
   cudaMemcpy(d_R, h_R, (m+2) * sizeof(char), cudaMemcpyHostToDevice);
 
   // Invoca o kernel
-  int threadsPerBlock;
-  if (n > 1024)
-    threadsPerBlock = 1024;
-  else
-    thredsPerBlock = n;
+  int threads_por_bloco = n;
+  if (n > MAX_THREADS)
+    threads_por_bloco = MAX_THREADS;
 
   InicializaVetor<<<1, 1>>>(n, m, d_D);
   cudaDeviceSynchronize();
 
-  DistanciaEdicao<<<1, threadsPerBlock>>>(n, m, d_S, d_R, d_D);
-
+  CalculaDistanciaEdicao<<<1, threads_por_bloco>>>(n, m, d_S, d_R, d_D);
   cudaDeviceSynchronize();
 
-  cudaMemcpy(&resultado, &d_D[tamanho_grid - 1], sizeof(int), cudaMemcpyDeviceToHost);
+  cudaMemcpy(&resultado, &d_D[tam_vetor - 1], sizeof(int), cudaMemcpyDeviceToHost);
   std::cout << resultado << std::endl;
 
-  // Libera matriz e strings
+  // Libera o vetor e as strings
   delete[] h_S;
   delete[] h_R;
   cudaFree(d_D);
   cudaFree(d_S);
   cudaFree(d_R);
+
   return 0;
 }
 
@@ -93,28 +93,39 @@ __global__ void InicializaVetor(int n, int m, int *D)
     D[j] = j;
 }
 
-__global__ void DistanciaEdicao(int n, int m, char *S, char *R, int *D)
+__global__ void CalculaDistanciaEdicao(int n, int m, char *S, char *R, int *D)
 {
+  int num_iteracoes = (n-1) / MAX_THREADS + 1;
+  int num_anti_diag, deslocamento;
   int a, b, c, t, min;
-  int nADiag = n + m - 1;
-  int i = threadIdx.x + 1, j;
+  int i, j;
 
-  // Para cada anti-diagonal
-	for (int aD = 2; aD <= nADiag + 1; aD++)
-	{
-    j = aD - i;
-    // Se é uma célula válida
-    if (j > 0 && j <= m)
+  for (int it = 0; it < num_iteracoes; ++it)
+  {
+    deslocamento = it * MAX_THREADS;
+    i = threadIdx.x + deslocamento + 1;
+
+    if (n - deslocamento > MAX_THREADS)
+      num_anti_diag = MAX_THREADS + m - 1;
+    else
+      num_anti_diag = n - deslocamento + m - 1;
+
+    for (int anti_diag = 2; anti_diag <= num_anti_diag + 1; anti_diag++)
     {
-      t = (S[i] != R[j] ? 1 : 0);
-      a = D[i*(m+1)+j-1] + 1;
-      b = D[(i-1)*(m+1)+j] + 1;
-      c = D[(i-1)*(m+1)+j-1] + t;
-      // Calcula d[i][j] = min(a, b, c)
-      min = (a < b ? a : b);
-      min = (c < min ? c : min);
-      D[i*(m+1)+j] = min;
+      j = anti_diag - (threadIdx.x + 1);
+      // Se é uma célula válida
+      if (i <= n && j > 0 && j <= m)
+      {
+        t = (S[i] != R[j] ? 1 : 0);
+        a = D[i*(m+1)+j-1] + 1;
+        b = D[(i-1)*(m+1)+j] + 1;
+        c = D[(i-1)*(m+1)+j-1] + t;
+
+        min = (a < b ? a : b);
+        min = (c < min ? c : min);
+        D[i*(m+1)+j] = min;
+      }
+      __syncthreads();
     }
-    __syncthreads();
   }
 }
